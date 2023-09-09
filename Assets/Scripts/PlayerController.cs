220f9fd280b4a9b21617d7c4aeb0ed8a0de22f0e
Deftoner555 <jmoya555@gmail.com>
2023-09-09 15:32:41 -0700

    clamped player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float MoveSpeed = 10f;
-
+    private float minX = -4f, maxX = -4f, minY = -3.44f, maxY = 5.5f;
 
     void Start()
     {
@@ -14,6 +14,10 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        
+        float myVal = Input.GetAxis("Mouse Y");
+        transform.Translate(0, myVal * MoveSpeed, 0);
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX, maxX),
+                  Mathf.Clamp(transform.position.y, minY, maxY), 6f);
     }
 }
