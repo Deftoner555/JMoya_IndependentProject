ef2a40a2b30771f7f5b67116fd8db708b469461e
Deftoner555 <jmoya555@gmail.com>
2023-09-08 17:03:47 -0700

    started movement script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MoveSpeed = 10f;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
