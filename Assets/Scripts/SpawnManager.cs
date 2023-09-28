3c6f633c64c1317338b16dfa895fcdaba8abcbc5
Deftoner555 <jmoya555@gmail.com>
2023-09-27 22:48:22 -0700

    housekeeping

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] NotePrefabs;
+
+    //set list of Y positions that prefabs should spawn at
     private float[] yPositions = { 5.84f, 2.5f, -1f, -4f };
+
     void Start()
     {
         InvokeRepeating("SpawnNotes", 2f, .5f); 
@@ -13,15 +16,10 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnNotes()
     {
+        //select a random Y position from the list
         float RandYPos = yPositions[Random.Range(0, yPositions.Length)];
+
         int NotePrefabIndex = Random.Range(0, NotePrefabs.Length);
         Instantiate(NotePrefabs[NotePrefabIndex], new Vector3(20, RandYPos, 6), NotePrefabs[NotePrefabIndex].transform.rotation);
     } 
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            
-        }
-    }
 }
