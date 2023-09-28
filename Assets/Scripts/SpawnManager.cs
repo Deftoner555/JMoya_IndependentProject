7e3d9a230c62ea275e525000923018a847a708c1
Deftoner555 <jmoya555@gmail.com>
2023-09-27 22:39:51 -0700

    Second phase done!

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,17 +8,20 @@ public class SpawnManager : MonoBehaviour
     private float[] yPositions = { 5.84f, 2.5f, -1f, -4f };
     void Start()
     {
-        
+        InvokeRepeating("SpawnNotes", 2f, .5f); 
     }
 
-     
+    void SpawnNotes()
+    {
+        float RandYPos = yPositions[Random.Range(0, yPositions.Length)];
+        int NotePrefabIndex = Random.Range(0, NotePrefabs.Length);
+        Instantiate(NotePrefabs[NotePrefabIndex], new Vector3(20, RandYPos, 6), NotePrefabs[NotePrefabIndex].transform.rotation);
+    } 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            float RandYPos = yPositions[Random.Range(0, yPositions.Length)];
-            int NotePrefabIndex = Random.Range(0, NotePrefabs.Length);
-            Instantiate(NotePrefabs[NotePrefabIndex], new Vector3 (20, RandYPos, 6), NotePrefabs[NotePrefabIndex].transform.rotation);
+            
         }
     }
 }
